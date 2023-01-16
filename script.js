d191ac322e593b36cc84a0311ee81ded76465b90
lukak1997 <khutsidzeluka@gmail.com>
2023-01-15 20:07:06 -0500

    first-js

---
+++ script.js
@@ -0,0 +1,27 @@
+// primitive - 7
+
+// stirng 
+let continent = 'europe';
+let country ='spain';
+console.log(continent,typeof continent);
+console.log(country,typeof country);
+
+
+
+// number
+const populationEurope = 742.65;
+const populationSpain = 47.08;
+console.log(populationEurope,typeof populationEurope);
+console.log(populationSpain,typeof populationSpain );
+
+
+
+//boolean - true false
+let spainIsIsland = false;
+console.log(spainIsIsland,typeof spainIsIsland)
+
+// undefined
+let largestContinent;
+console.log(largestContinent,typeof largestContinent);
+largestContinent ='asia';
+console.log(largestContinent);
